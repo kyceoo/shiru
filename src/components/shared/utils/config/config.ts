14bfc46325a85a7956e2c6e569cc123cc38000d9
kyceoo <paulngaruiya714@gmail.com>
2025-12-23 07:12:26 +0000

    Update app loader styles and config files

--- src/components/shared/utils/config/config.ts
+++ src/components/shared/utils/config/config.ts
@@ -7,9 +7,9 @@ export const APP_IDS = {
     STAGING: 29934,
     STAGING_BE: 29934,
     STAGING_ME: 29934,
-    PRODUCTION: 65555,
-    PRODUCTION_BE: 65556,
-    PRODUCTION_ME: 65557,
+    PRODUCTION: 117748,
+    PRODUCTION_BE: 117748,
+    PRODUCTION_ME: 117748,
 };
 
 export const livechat_license_id = 12049137;
